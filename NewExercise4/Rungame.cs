d90d77602b6e4e3d1969522cee923867329fc4a7
Julio Juarez <julio.juarez5@outlook.com>
2018-01-30 12:04:40 -0500

    add  Run, UserInput,ActOnSelectedItem, and WelcomeMenu methods inside RunGame.

--- NewExercise4/Rungame.cs
+++ NewExercise4/Rungame.cs
@@ -8,6 +8,28 @@ namespace NewExercise4
 {
     class RunGame
     {
+        public void Run()
+        {
+            var exitProgram = false;
+
+            do
+            {
+                WelcomeMenu();
+                exitProgram = ActOnSelectedItem(UserInput());
+
+            } while (!exitProgram);
+        }
+
+        private object UserInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool ActOnSelectedItem(object v)
+        {
+            throw new NotImplementedException();
+        }
+
         // Method to Display the Welcome menu for the game
         // The Welcome Menu should display
         // -Welcome to Space Explorer 1982!
@@ -15,7 +37,13 @@ namespace NewExercise4
         // Choose an option :
         // 1. New Game
         // 0. Exit
+        public void WelcomeMenu()
+        {
+            Console.WriteLine(" Welcome to Space Explorer 1983!\n");
+            Console.WriteLine(" Choose an option :");
+            Console.WriteLine(" 1. New Game");
+            Console.WriteLine(" 0. Exit \n");
+        }
         // try/catch exception handling will go here too
-
     }
 }
