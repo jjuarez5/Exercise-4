502dc03631fe93b16faa1dd29d205fe1f603bb66
Julio Juarez <julio.juarez5@outlook.com>
2018-01-30 11:24:04 -0500

    add pseudocode for the welcome menu to the RunGame class

--- NewExercise4/Rungame.cs
+++ NewExercise4/Rungame.cs
@@ -8,8 +8,14 @@ namespace NewExercise4
 {
     class RunGame
     {
-        //Method to Display the main menu for the game
-        //try/catch exception handling will go here too
+        // Method to Display the Welcome menu for the game
+        // The Welcome Menu should display
+        // -Welcome to Space Explorer 1982!
+        // __________________________________
+        // Choose an option :
+        // 1. New Game
+        // 0. Exit
+        // try/catch exception handling will go here too
 
     }
 }
