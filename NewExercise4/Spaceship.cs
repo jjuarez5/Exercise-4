ac1dac5615828d93ba11cd9785e5e221d17edeec
Greg Wilson <greg.wilson5@outlook.com>
2018-02-06 00:18:39 -0500

    show created methods for onloading and offloading supplies.

--- NewExercise4/Spaceship.cs
+++ NewExercise4/Spaceship.cs
@@ -8,12 +8,38 @@ namespace NewExercise4
 {
     class Spaceship
     {
-        string currentShip = ("EarthWorm");
+        public string currentShip = ("EarthWorm Jim");
         public string myShip;
+        public int supplies = 0;
+
         public string determineShip()
         {
             myShip = currentShip;
             return myShip;
+        }
+
+        // code structure by argiopetech
+        public void InboundSupplies(int onLoadedSupplies)
+        {
+            this.supplies += onLoadedSupplies;
+        }
+
+        // code structure by argiopetech
+        public int OutboundSupplies(int offLoadedSupplies)
+        {
+            int suppliesSold;
+
+            if (offLoadedSupplies > this.supplies)
+            {
+                suppliesSold = this.supplies;
+                this.supplies = 0;
+            }
+            else
+            {
+                suppliesSold = offLoadedSupplies;
+                this.supplies -= offLoadedSupplies;
+            }
+            return suppliesSold;
         }
     }
 }
